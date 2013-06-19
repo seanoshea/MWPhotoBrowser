25d3b21f2dfd80ff062ebe2695876f4386b65fc9
seanoshea <oshea.ie@gmail.com>
2013-06-19 22:31:09 +0100

    Added an additional delegate method.

--- MWPhotoBrowser/Classes/MWPhotoBrowser.m
+++ MWPhotoBrowser/Classes/MWPhotoBrowser.m
@@ -874,6 +874,10 @@ navigationBarBackgroundImageLandscapePhone = _navigationBarBackgroundImageLandsc
 	} else {
 		self.title = nil;
 	}
+    
+    if ([_delegate respondsToSelector:@selector(navigationUpdatedWithCurrentIndex:)]) {
+        [_delegate navigationUpdatedWithCurrentIndex:_currentPageIndex];
+    }
 	
 	// Buttons
 	_previousButton.enabled = (_currentPageIndex > 0);
